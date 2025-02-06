6105c31cbdf8259237dc61fa00283ecc36df83c0
Nadia Mirza-Saadi <nadiamirzasaadi@gmail.com>
2025-02-06 11:25:50 +0000

    Update Assignment1.py
    Ran pylint to check the coding standard. Added a docustring in line 2, simplified the docustring in line 18, used an f-string to quickly print the value instead of running through all the workers outputs in line 59. I removed the comment "# Added a docustring to explain the purpose, input and output of the function
    #Technically it should return either no value or a worker will find the next value of pi." as this belongs here and also initially improved the pylint score and allowed the code to run smoothly. Considered using fsum instead of mpmath to improve precision and speed.

--- Assignment1.py
+++ Assignment1.py
@@ -1,8 +1,17 @@
 #!/bin/python3
+"""
+	Sets environment for program to work in, including the conda environment: condampipython.
+	Args: 
+		python3
+	Returns: 
+		mpmath, mpi4py, math.fsum
+"""
 from mpi4py import MPI
 from mpmath import mp, mpf
+#from math.fsum import fsum
 
 comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
 nproc = comm.Get_size()
 # The first processor is leader, so one fewer available to be a worker
 nworkers = nproc - 1
@@ -11,46 +20,43 @@ N = 100000000
 DELTA = mpf(1.0) / N  # used mpf to calculate a precise value of pi
 # integral
 I = mpf(0.0)  # maintained this method throughout the calculation.
-mp.dps = 14  # I have initially set the d.p.=14 to quickly test this 
-#version works, I will gradually increase it as I alter my code to keep it in quick parallel.
+mp.dps = 14 # I have initially set the d.p.=2 to quickly test this 
+#version works, I will gradually increase it as I alter my code to keep 
+#it in quick parallel.
+#Have now moved to 14 d.p.
 
 def integrand(x):
-# Added a docustring to explain the purpose, input and output of the function
-#Technically it should return either no value or a worker will find the next value of pi.
     """
     Function to solve pi.
 
     Args: 
-        x: Works out the value of pi, `mpf()` helps with accuracy and speeds this up.
-        DELTA: Carries out the integration.
-
+        x (mpf): x is evaluated by the workers and fed back through a 
+        loop in parallel with each other.
+        
     Returns:
-        None / DELTA
+        mpf: the value of pi worked out by the integrand function.
     """
     return mpf(4.0) / (mpf(1.0) + x * x)
-
-if comm.Get_rank() == 0:  # Leader: choose points to sample function, send to workers and
+if comm.Get_rank() == 0:    # Leader: choose points to sample function, send to workers and
     # collect their contributions. Also calculate a sub-set of points.
     for i in range(0, N):
         # decide which rank evaluates this point
         j = i % nproc
         # mid-point rule
-        x = (i + 0.5) * DELTA
+        recv_x = (i + 0.5) * DELTA
         if j == 0:
             # so do this locally using the leader machine
-            y = integrand(x) * DELTA
+            y = integrand(recv_x) * DELTA
         else:
             # communicate to a worker
-            comm.send(x, dest=j)
+            comm.send(recv_x, dest=j)
             y = comm.recv(source=j)
         I += y
 
     # Shut down the workers
     for i in range(1, nproc):
         comm.send(-1.0, dest=i)
-
-    # Only rank 0 should print the result
-    print(f"The value of pi to 15 s.f. = {I}")
+    print(f"The value of pi to 15 s.f. = {I:.14f}")
 
 else:
     # Worker: waiting for something to happen, then stop if sent message
@@ -61,4 +67,3 @@ else:
             # stop the worker
             break
         comm.send(integrand(recv_x) * DELTA, dest=0)
-# I have corrected the indentations here too.
