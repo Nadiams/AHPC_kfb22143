5433a8828986899908caae808ffad034dce99bfb
Nadia Mirza-Saadi <nadiamirzasaadi@gmail.com>
2025-02-10 19:49:10 +0000

    Update Assignment2.py
    Also added the subtraction OOP and then to calculate magnitude of the vector function (v1) the object holds. Used the identity matrix here.

--- Assignment2.py
+++ Assignment2.py
@@ -6,47 +6,42 @@ Created on Mon Feb 10 14:01:27 2025
 @author: nadia
 """
 
+import math
+import numpy as np
+
 class Vector:
     """
-        Vector class for Cartesian vectors in 3D space
+        Vector class for Cartesian vectors in 3D space.
     """
     def __init__(self, i, j, k):
         self.i = i
         self.j = j
         self.k = k
-    
-    def __str__(self):
-        """
-        Assumes floating point when printing
-        """
-        return f"Vector: ({self.i:2f}, {self.j:2f}, {self.k:2f})"
 
-    def __add__(self,other):
-        """
-        Adds the strings together to create vector.
-        """
+    def __str__(self):
+        return f"({self.i}, {self.j}, {self.k})"
+
+    def __add__(self, other):
         return Vector(self.i + other.i, self.j + other.j, self.k + other.k)
 
-    def norm(self):
-        """computes magnitude of vector"""
-        return math.sqrt(self.i**2+self.j**2+self.k**2)
+    def __sub__(self, other):
+        return Vector(self.i - other.i, self.j - other.j, self.k - other.k)
 
+    def norm(self):
+        """Computes magnitude of the vector"""
+        return math.sqrt(self.i**2 + self.j**2 + self.k**2)
 
     def __array__(self, dtype=None):
         if dtype:
             return np.array([self.i, self.j, self.k], dtype=dtype)
         else:
             return np.array([self.i, self.j, self.k])
-        
-#i = Vector(1.,0.,0.)
-#k = Vector(0.,0.,1.)
 
-#j = Vector(0.,1.,0.)
+v1 = Vector(1, 0, 0)
+v2 = Vector(0, 1, 0)
 
-#print(i)
-#print(j)
-#print(k)
-
-ijk = Vector()
-
-print()
+print("Vector v1:", v1)
+print("Vector v2:", v2)
+print("Magnitude of v1:", v1.norm())
+print("v1 + v2:", v1 + v2)
+print("v1 - v2:", v1 - v2)
