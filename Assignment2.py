7860d62a8ce04f4a328e425365e4841fc6816f72
Nadia Mirza-Saadi <nadiamirzasaadi@gmail.com>
2025-02-12 17:19:23 +0000

    Update Assignment2.py
    Improved pylint score by adding docustrings, removing trailing whitespace, correcting indentation, removed one else statement and de-indented code inside, corrected docustrings, reduced a line length and removed excess spacing.

--- Assignment2.py
+++ Assignment2.py
@@ -13,27 +13,51 @@ class Vector:
         Vector class for Cartesian vectors in 3D space.
     """
     def __init__(self, i, j, k):
+        """
+            Initializes vector with i, j and k components.
+        """
         self._i = i
         self._j = j
         self._k = k
 
     def __str__(self):
+        """
+            String representation of Cartesian vector.
+        """
         return f"({self._i:.2f}, {self._j:.2f}, {self._k:.2f})"
 
     def __add__(self, other):
+        """
+            Args: vector components
+            Returns: Cartesian vector
+            Adds vectors.
+        """
         return Vector(self._i + other._i, self._j + other._j, self._k + other._k)
 
     def __sub__(self, other):
+        """
+            Args: vector components
+            Returns: Cartesian vector
+            Subtracts vectors.
+        """
         return Vector(self._i - other._i, self._j - other._j, self._k - other._k)
 
     def norm(self):
-        """Calculates the magnitude of the vector"""
+        """
+            Calculates the magnitude of the vector.
+        """
         return math.sqrt(self._i**2 + self._j**2 + self._k**2)
 
     def dot(self, other):
+        """
+            Calculates the dot product of two vectors.
+        """
         return self._i * other._i + self._j * other._j + self._k * other._k
 
     def cross(self, other):
+        """
+            Calculates the cross product of two vectors.
+        """
         return Vector(
             self._j * other._k - self._k * other._j,
             self._k * other._i - self._i * other._k,
@@ -41,10 +65,12 @@ class Vector:
         )
 
     def __array__(self, dtype=None):
+        """
+            Creates an array to contain the vector components, to display.
+        """
         if dtype:
             return np.array([self._i, self._j, self._k], dtype=dtype)
-        else:
-            return np.array([self._i, self._j, self._k])
+        return np.array([self._i, self._j, self._k])
 
     def cartesian_to_spherical(self):
         """
@@ -55,25 +81,39 @@ class Vector:
         phi = math.atan2(self._j, self._i)
 
         return SphericalPolarVector(r, theta, phi)
-    
+
 class SphericalPolarVector(Vector):
     """
-    Using inheritance to take previous methods used in parent class to pass to child class.
+    Uses inheritance to take previous methods used in parent class to pass to child class.
     """
     def __init__(self, r, theta, phi):
+        """
+            Initialises the vector with r, theta and phi components to convert
+            from cartesian i, j and k components.
+            Args: 
+                i, j and k
+            Returns: 
+                r, theta and phi.
+        """
         self._r = r
         self._theta = theta
         self._phi = phi
-        
+
         i = r * math.sin(theta) * math.cos(phi)  # x-component
         j = r * math.sin(theta) * math.sin(phi)  # y-component
         k = r * math.cos(theta)  # z-component
-        
+
         super().__init__(i, j, k)
 
     def __str__(self):
-        return f"(r={self._r:.2f}, θ={math.degrees(self._theta):.2f}°, φ={math.degrees(self._phi):.2f}°)"
-
+        """
+        String representation of the vector in spherical-polar form.
+        """
+        return (
+            f"(r={self._r:.2f}, "
+            f"θ={math.degrees(self._theta):.2f}°, "
+            f"φ={math.degrees(self._phi):.2f}°)"
+    )
 v1 = Vector(1, 0, 0)  # i=1, j=0, k=0
 v2 = Vector(0, 1, 0)  # i=0, j=1, k=0
 v3 = Vector(0, 0, 1)  # i=0, j=0, k=1
