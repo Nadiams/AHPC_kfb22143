119873eee75694fc143ad247a0efc67d151e716b
Nadia Mirza-Saadi <nadiamirzasaadi@gmail.com>
2025-03-03 22:09:10 +0000

    Plotting 2D and 3D results.
    Added the plots as functions to call on to keep with assignment requirements of using Object Oriented Programming but also in case of tasks being required in the future.
    
    Specified when the plots are to be printed.

--- assignment3.py
+++ assignment3.py
@@ -16,7 +16,7 @@ from numpy.random import SeedSequence, default_rng
 #from mpi4py import MPI
 
 class MonteCarlo:
-    def __init__(self, num_samples=1000000, dimensions=6, seed=12345):
+    def __init__(self, num_samples=10000, dimensions=6, seed=12345):
         """
         To initialize the Monte Carlo class.
         """
@@ -47,10 +47,50 @@ class MonteCarlo:
         cube_volume = 2**self.dimensions
         return volume_fraction * cube_volume
 
+    def twodimensionscatter(self):
+        """
+        Visualise sampled points in 2D.
+        """
+        points = self.sample_points()
+        inside = self.in_hypersphere(points)
+
+        plt.figure(figsize=(6, 6))
+        plt.scatter(points[0][inside], points[1][inside], color='blue', label='Inside Circle', s=1)
+        plt.scatter(points[0][~inside], points[1][~inside], color='red', label='Outside Circle', s=1)
+        plt.legend()
+        plt.xlabel("x-axis")
+        plt.ylabel("y-axis")
+        plt.title("Monte Carlo Sampling of a 2D Circle")
+        plt.grid()
+        
+    def threedimensionscatter(self):
+        """
+            Visualise sampled points in 3D.
+        """
+        points = self.sample_points()
+        inside = self.in_hypersphere(points)
+
+        fig = plt.figure(figsize=(8, 8))
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(points[0][inside], points[1][inside], points[2][inside], 'bo', label='Inside Sphere', markersize=1)
+        ax.plot(points[0][~inside], points[1][~inside], points[2][~inside], 'ro', label='Outside Sphere', markersize=1)
+        ax.set_xlabel("x-axis")
+        ax.set_ylabel("y-axis")
+        ax.set_zlabel("z-axis")
+        ax.set_title("Monte Carlo Sampling of a 3D Sphere")
+        ax.legend()
+
 if __name__ == "__main__":
-    num_samples = 1000000
-    dimensions = [2, 3, 4, 5, 6]
-    for d in dimensions:
-        mc_simulator = MonteCarlo(num_samples, d)
+    num_samples = 10000
+    dimensions_list = [2, 3, 4, 5, 6]
+    mc_results = []
+
+    for d in dimensions_list:
+        mc_simulator = MonteCarlo(num_samples=num_samples, dimensions=d)
         volume_estimate = mc_simulator.estimate_volume()
+        mc_results.append(volume_estimate)
         print(f"Estimated volume for {d}D hypersphere: {volume_estimate:.6f}")
+        if d == 2:
+            mc_simulator.twodimensionscatter()
+        elif d == 3:
+            mc_simulator.threedimensionscatter()
