31e13e5f9aaf4ef9edf6179835677a205855a72d
Nadia Mirza-Saadi <nadiamirzasaadi@gmail.com>
2025-03-09 19:13:12 +0000

    Placement of parallel code.
    Added parallel code higher so that I can see it better to work from.

--- assignment3.py
+++ assignment3.py
@@ -40,6 +40,34 @@ class MonteCarloIntegrator:
         self.rank = self.comm.Get_rank()
         self.size = self.comm.Get_size()
         self.rng = default_rng(seed=self.rank)
+def parallelmontecarlo(num_samples, dimensions):
+    """
+        Class to use MPI Parallelism.
+    """
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    mc_simulator = ContainedRegion(num_samples=num_samples, dimensions=dimensions)
+    lower_bounds = np.array([-1] * dimensions, dtype=float)
+    upper_bounds = np.array([1] * dimensions, dtype=float)
+    volume = np.prod(upper_bounds - lower_bounds)
+    local_samples = num_samples // size
+    rng = np.random.default_rng(seed=rank)
+    count_inside = 0
+    for i in range(local_samples):
+        point = rng.uniform(-1, 1, dimensions)
+        if np.sum(point**2) <= 1:
+            count_inside += 1
+    region_volume = (2**dimensions) * (count_inside / local_samples)
+    total_volumes = comm.gather(region_volume, root=0)
+    if rank == 0:
+        for i in range(0, dimensions):
+            mean_volume = np.mean(total_volumes)
+            variance = np.var(total_volumes)
+            print(
+                f"The {dimensions}D Hyperspace Volume: {mean_volume:.6f} "
+                f"± {np.sqrt(variance):.6f}"
+            )
 
     def integrate(self):
         """
